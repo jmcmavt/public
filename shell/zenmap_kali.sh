7b2e42788894a7268c42d097e990e72282917f3c
jmcmavt <96273068+jmcmavt@users.noreply.github.com>
2023-09-21 22:19:07 -0400

    Update zenmap_kali.sh

--- shell/zenmap_kali.sh
+++ shell/zenmap_kali.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-apt update -y
-apt-get upgrade -y
 mkdir ~/rpm
 cd ~/rpm
 wget https://nmap.org/dist/zenmap-7.94-1.noarch.rpm
-apt install alien -y
 alien zenmap-7.94-1.noarch.rpm
 wget http://archive.ubuntu.com/ubuntu/pool/universe/p/pygtk/python-gtk2_2.24.0-5.1ubuntu2_amd64.deb
 wget http://azure.archive.ubuntu.com/ubuntu/pool/universe/p/pygobject-2/python-gobject-2_2.28.6-14ubuntu1_amd64.deb
